d37d81a9bb916214aac89e1ed26b0350cccb0f14
KYovchevski <kamen.yovchevski@traverseresearch.nl>
2024-01-10 13:32:51 +0100

    Finish IADLXSystem initialization

--- src/adlx/helper.rs
+++ src/adlx/helper.rs
@@ -1,4 +1,4 @@
-use std::{ffi::CStr, mem::MaybeUninit};
+use std::ffi::CStr;
 
 use anyhow::{ensure, Context, Result};
 
@@ -6,18 +6,26 @@ use super::{result::Error, system::System};
 use crate::bindings as ffi;
 
 // TODO: This should be a singleton
-pub struct AdlxHelper {
-    // Keepalive for the loaded library
-    #[allow(dead_code)]
+
+struct AdlxFunctions {
     lib: libloading::Library,
-    system: super::system::System,
+
+    full_version_fn: ffi::ADLXQueryFullVersion_Fn,
+    version_fn: ffi::ADLXQueryVersion_Fn,
+    init_with_adl_fn: ffi::ADLXInitializeWithCallerAdl_Fn,
+    init_with_incompatible_driver_fn: ffi::ADLXInitialize_Fn,
+    init_fn: ffi::ADLXInitialize_Fn,
     terminate_fn: ffi::ADLXTerminate_Fn,
-    // query_version_fn: ffi::ADLXQueryVersion_Fn,
-    // query_full_version_fn: ffi::ADLXQueryFullVersion_Fn,
 }
 
-impl AdlxHelper {
-    pub fn new() -> Result<Self> {
+impl AdlxFunctions {
+    unsafe fn new() -> anyhow::Result<Self> {
+        let dll_name = CStr::from_bytes_with_nul(ffi::ADLX_DLL_NAME)
+            .unwrap()
+            .to_str()
+            .unwrap();
+        let lib = libloading::Library::new(dll_name).context("Failed to load amdadlx DLL")?;
+
         fn load_symbol<T: Copy>(lib: &libloading::Library, name: &[u8]) -> Result<Option<T>> {
             let name_c = CStr::from_bytes_with_nul(name)?;
             let sym: Option<T> = *unsafe { lib.get(name) }
@@ -27,36 +35,84 @@ impl AdlxHelper {
             Ok(sym)
         }
 
-        unsafe {
-            // libloading requires an OsString, not a CStr.
-            let dll_name = CStr::from_bytes_with_nul(ffi::ADLX_DLL_NAME)
-                .unwrap()
-                .to_str()
-                .unwrap();
-            let lib = libloading::Library::new(dll_name)
-                .with_context(|| format!("Failed to load `{dll_name}`"))?;
+        let full_version_fn: ffi::ADLXQueryFullVersion_Fn = load_symbol(
+            &lib,
+            ffi::ADLX_QUERY_FULL_VERSION_FUNCTION_NAME,
+        )?;
+        let version_fn: ffi::ADLXQueryVersion_Fn = load_symbol(
+            &lib,
+            ffi::ADLX_QUERY_VERSION_FUNCTION_NAME,
+        )?;
+        let init_with_adl_fn: ffi::ADLXInitializeWithCallerAdl_Fn = load_symbol(
+            &lib,
+            ffi::ADLX_INIT_WITH_CALLER_ADL_FUNCTION_NAME,
+        )?;
+        let init_with_incompatible_driver_fn: ffi::ADLXInitialize_Fn = load_symbol(
+            &lib,
+            ffi::ADLX_INIT_WITH_INCOMPATIBLE_DRIVER_FUNCTION_NAME,
+        )?;
+        let init_fn: ffi::ADLXInitialize_Fn = load_symbol(&lib, ffi::ADLX_INIT_FUNCTION_NAME)?;
+        let terminate_fn: ffi::ADLXTerminate_Fn =
+            load_symbol(&lib, ffi::ADLX_TERMINATE_FUNCTION_NAME)?;
 
-            let init_fn: ffi::ADLXInitialize_Fn = load_symbol(&lib, ffi::ADLX_INIT_FUNCTION_NAME)?;
-            let terminate_fn: ffi::ADLXTerminate_Fn =
-                load_symbol(&lib, ffi::ADLX_TERMINATE_FUNCTION_NAME)?;
-            // let query_version_fn: ffi::ADLXQueryVersion_Fn =
-            //     load_symbol(&lib, ffi::ADLX_QUERY_VERSION_FUNCTION_NAME)?;
-            // let query_full_version_fn: ffi::ADLXQueryFullVersion_Fn =
-            //     load_symbol(&lib, ffi::ADLX_QUERY_FULL_VERSION_FUNCTION_NAME)?;
-            let mut system = MaybeUninit::uninit();
-            let result = (init_fn.unwrap_unchecked())(1337, system.as_mut_ptr());
-            let system = Error::from_result_with_assume_init_on_success(result, system)?;
-            let system = System::from_raw(system);
+        Ok(Self {
+            lib,
 
-            // TODO: Lifetime all returned interfaces such that they _CANNOT_ outlive AdlxHelper, which has the library open!
-            Ok(Self {
-                lib,
-                system,
-                terminate_fn,
-                // query_version_fn,
-                // query_full_version_fn,
-            })
-        }
+            full_version_fn,
+            version_fn,
+            init_with_adl_fn,
+            init_with_incompatible_driver_fn,
+            init_fn,
+            terminate_fn,
+        })
+    }
+}
+
+pub struct AdlxHelper {
+    functions: AdlxFunctions,
+
+    system: super::system::System,
+
+    full_version: u64,
+    version: String,
+}
+
+impl AdlxHelper {
+    pub fn new() -> Result<Self> {
+        let functions = unsafe { AdlxFunctions::new()? };
+
+        let full_version = unsafe {
+            let mut full_version = 0;
+            Error::from_result((functions.full_version_fn.unwrap())(&mut full_version))?;
+            full_version
+        };
+
+        let version = unsafe {
+            let mut version = std::ptr::null();
+            Error::from_result((functions.version_fn.unwrap())(&mut version))?;
+            CStr::from_ptr(version).to_str()?.to_string()
+        };
+
+        // TODO: C++ helper does extra things if an ADL context is provided.
+        // We don't support this currently because we are only implementing ADLX
+        let system = unsafe {
+            let mut system = std::ptr::null_mut();
+            let init_full_version = ((ffi::ADLX_VER_MAJOR as u64) << 48)
+                | ((ffi::ADLX_VER_MINOR as u64) << 32)
+                | ((ffi::ADLX_VER_RELEASE as u64) << 16)
+                | (ffi::ADLX_VER_BUILD_NUM as u64);
+            Error::from_result((functions.init_fn.unwrap())(init_full_version, &mut system))?;
+
+            System::from_raw(system)
+        };
+        dbg!(&version);
+        Ok(AdlxHelper {
+            functions,
+
+            full_version,
+            version,
+            system,
+        })
     }
 
     pub fn system(&self) -> &System {
@@ -67,7 +123,7 @@ impl AdlxHelper {
 impl Drop for AdlxHelper {
     fn drop(&mut self) {
         // SAFETY: Nullity checked at load-time
-        let result = unsafe { (self.terminate_fn.unwrap_unchecked())() };
+        let result = unsafe { (self.functions.terminate_fn.unwrap_unchecked())() };
         if let Err(e) = Error::from_result(result) {
             eprintln!("Terminate failed with {e:?}")
         }
--- src/bindings.rs
+++ src/bindings.rs
@@ -4,3 +4,32 @@
 #![allow(dead_code)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+pub(crate) fn adlx_result_to_error(res: ADLX_RESULT) -> anyhow::Result<()> {
+    match res {
+        ADLX_RESULT_ADLX_OK => Ok(()),
+        ADLX_RESULT_ADLX_ALREADY_ENABLED => {
+            Err(anyhow::anyhow!("ADLX_RESULT_ADLX_ALREADY_ENABLED"))
+        }
+        ADLX_RESULT_ADLX_ALREADY_INITIALIZED => {
+            Err(anyhow::anyhow!("ADLX_RESULT_ADLX_ALREADY_INITIALIZED"))
+        }
+        ADLX_RESULT_ADLX_FAIL => Err(anyhow::anyhow!("ADLX_RESULT_ADLX_FAIL")),
+        ADLX_RESULT_ADLX_INVALID_ARGS => Err(anyhow::anyhow!("ADLX_RESULT_ADLX_INVALID_ARGS")),
+        ADLX_RESULT_ADLX_BAD_VER => Err(anyhow::anyhow!("ADLX_RESULT_ADLX_BAD_VER")),
+        ADLX_RESULT_ADLX_UNKNOWN_INTERFACE => {
+            Err(anyhow::anyhow!("ADLX_RESULT_ADLX_UNKNOWN_INTERFACE"))
+        }
+        ADLX_RESULT_ADLX_TERMINATED => Err(anyhow::anyhow!("ADLX_RESULT_ADLX_TERMINATED")),
+        ADLX_RESULT_ADLX_ADL_INIT_ERROR => Err(anyhow::anyhow!("ADLX_RESULT_ADLX_ADL_INIT_ERROR")),
+        ADLX_RESULT_ADLX_NOT_FOUND => Err(anyhow::anyhow!("ADLX_RESULT_ADLX_NOT_FOUND")),
+        ADLX_RESULT_ADLX_INVALID_OBJECT => Err(anyhow::anyhow!("ADLX_RESULT_ADLX_INVALID_OBJECT")),
+        ADLX_RESULT_ADLX_ORPHAN_OBJECTS => Err(anyhow::anyhow!("ADLX_RESULT_ADLX_ORPHAN_OBJECTS")),
+        ADLX_RESULT_ADLX_NOT_SUPPORTED => Err(anyhow::anyhow!("ADLX_RESULT_ADLX_NOT_SUPPORTED")),
+        ADLX_RESULT_ADLX_PENDING_OPERATION => {
+            Err(anyhow::anyhow!("ADLX_RESULT_ADLX_PENDING_OPERATION"))
+        }
+        ADLX_RESULT_ADLX_GPU_INACTIVE => Err(anyhow::anyhow!("ADLX_RESULT_ADLX_GPU_INACTIVE")),
+        _ => Err(anyhow::anyhow!("Unknown ADLX_RESULT error")),
+    }
+}
