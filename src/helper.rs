1c26327106caa5177c47ee9dda4630ec0a2b2cb9
KYovchevski <kamen.yovchevski@traverseresearch.nl>
2024-01-10 15:17:17 +0100

    fmt

--- src/helper.rs
+++ src/helper.rs
@@ -2,7 +2,10 @@ use std::ffi::CStr;
 
 use anyhow::Context;
 
-use crate::bindings::{self, adlx_result_to_error, ADLX_VER_MAJOR, ADLX_VER_MINOR, ADLX_VER_RELEASE, ADLX_VER_BUILD_NUM};
+use crate::bindings::{
+    self, adlx_result_to_error, ADLX_VER_BUILD_NUM, ADLX_VER_MAJOR, ADLX_VER_MINOR,
+    ADLX_VER_RELEASE,
+};
 
 struct AdlxFunctions {
     lib: libloading::Library,
@@ -15,39 +18,42 @@ struct AdlxFunctions {
     terminate_fn: bindings::ADLXTerminate_Fn,
 }
 
-unsafe fn load_adlx_dll() -> anyhow::Result<AdlxFunctions>  {
-    let dll_name = CStr::from_bytes_with_nul(bindings::ADLX_DLL_NAME).unwrap().to_str().unwrap();
+unsafe fn load_adlx_dll() -> anyhow::Result<AdlxFunctions> {
+    let dll_name = CStr::from_bytes_with_nul(bindings::ADLX_DLL_NAME)
+        .unwrap()
+        .to_str()
+        .unwrap();
     let lib = libloading::Library::new(dll_name).context("Failed to load amdadlx DLL")?;
 
-        let full_version_fn = *lib
-            .get(bindings::ADLX_QUERY_FULL_VERSION_FUNCTION_NAME)
-            .context("Failed to get full version function")?;
-        let version_fn = *lib
-            .get(bindings::ADLX_QUERY_VERSION_FUNCTION_NAME)
-            .context("Failed to get version function")?;
-        let init_with_adl_fn = *lib
-            .get(bindings::ADLX_INIT_WITH_CALLER_ADL_FUNCTION_NAME)
-            .context("Failed to get initWithAdl function")?;
-        let init_with_incompatible_driver_fn = *lib
-            .get(bindings::ADLX_INIT_WITH_INCOMPATIBLE_DRIVER_FUNCTION_NAME)
-            .context("Failed to get init with incompatible driver function")?;
-        let init_fn = *lib
-            .get(bindings::ADLX_INIT_FUNCTION_NAME)
-            .context("Failed to get init function")?;
-        let terminate_fn = *lib
-            .get(bindings::ADLX_TERMINATE_FUNCTION_NAME)
-            .context("Failed to get terminate function")?;
+    let full_version_fn = *lib
+        .get(bindings::ADLX_QUERY_FULL_VERSION_FUNCTION_NAME)
+        .context("Failed to get full version function")?;
+    let version_fn = *lib
+        .get(bindings::ADLX_QUERY_VERSION_FUNCTION_NAME)
+        .context("Failed to get version function")?;
+    let init_with_adl_fn = *lib
+        .get(bindings::ADLX_INIT_WITH_CALLER_ADL_FUNCTION_NAME)
+        .context("Failed to get initWithAdl function")?;
+    let init_with_incompatible_driver_fn = *lib
+        .get(bindings::ADLX_INIT_WITH_INCOMPATIBLE_DRIVER_FUNCTION_NAME)
+        .context("Failed to get init with incompatible driver function")?;
+    let init_fn = *lib
+        .get(bindings::ADLX_INIT_FUNCTION_NAME)
+        .context("Failed to get init function")?;
+    let terminate_fn = *lib
+        .get(bindings::ADLX_TERMINATE_FUNCTION_NAME)
+        .context("Failed to get terminate function")?;
 
-        Ok(AdlxFunctions {
-            lib,
+    Ok(AdlxFunctions {
+        lib,
 
-            full_version_fn,
-            version_fn,
-            init_with_adl_fn,
-            init_with_incompatible_driver_fn,
-            init_fn,
-            terminate_fn,
-        })
+        full_version_fn,
+        version_fn,
+        init_with_adl_fn,
+        init_with_incompatible_driver_fn,
+        init_fn,
+        terminate_fn,
+    })
 }
 
 pub struct AdlxHelper {
@@ -61,35 +67,37 @@ pub struct AdlxHelper {
 impl Drop for AdlxHelper {
     fn drop(&mut self) {
         unsafe {
-            adlx_result_to_error((self.functions.terminate_fn.unwrap())()).expect("Failed to terminate ADLX");
+            adlx_result_to_error((self.functions.terminate_fn.unwrap())())
+                .expect("Failed to terminate ADLX");
         }
     }
 }
 
 pub fn init_helper() -> anyhow::Result<AdlxHelper> {
-    let functions = unsafe {
-        load_adlx_dll()?
-    };
+    let functions = unsafe { load_adlx_dll()? };
 
     let mut full_version = 0;
 
     unsafe {
         adlx_result_to_error((functions.full_version_fn.unwrap())(&mut full_version))?;
     }
-    
+
     let version = unsafe {
         let mut version = std::ptr::null();
         adlx_result_to_error((functions.version_fn.unwrap())(&mut version))?;
         CStr::from_ptr(version).to_str()?.to_string()
     };
-    
-    // TODO: C++ helper does extra things if an ADL context is provided. 
+
+    // TODO: C++ helper does extra things if an ADL context is provided.
     // We don't support this currently because we are only implementing ADLX
     let system = unsafe {
         let mut system = std::ptr::null_mut();
-        let init_full_version = ((ADLX_VER_MAJOR as u64) << 48) | ((ADLX_VER_MINOR as u64) << 32) | ((ADLX_VER_RELEASE as u64) << 16)  | (ADLX_VER_BUILD_NUM as u64);
+        let init_full_version = ((ADLX_VER_MAJOR as u64) << 48)
+            | ((ADLX_VER_MINOR as u64) << 32)
+            | ((ADLX_VER_RELEASE as u64) << 16)
+            | (ADLX_VER_BUILD_NUM as u64);
         adlx_result_to_error((functions.init_fn.unwrap())(init_full_version, &mut system))?;
-        
+
         Box::from_raw(system)
     };
     dbg!(&version);
