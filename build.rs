45ab3fc54256008bdaca3d38374cef2886c4c82c
KYovchevski <kamen.yovchevski@traverseresearch.nl>
2024-01-25 15:49:05 +0100

    Make binding generation an optinal feature
    Use --features=bindgen

--- build.rs
+++ build.rs
@@ -1,17 +1,21 @@
-use std::{env, path::PathBuf};
 
 fn main() {
-    println!("cargo:rerun-if-changed=src/adlx/wrapper.h");
+    #[cfg(feature = "bindgen")]
+    {
+        use std::path::PathBuf;
 
-    let bindings = bindgen::Builder::default()
-        .header("src/adlx/wrapper.h")
-        .allowlist_item("I?ADLX\\w+")
-        .generate()
-        .expect("failed to generate adlx bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        println!("cargo:rerun-if-changed=src/adlx/wrapper.h");
+    
+        let bindings = bindgen::Builder::default()
+            .header("src/adlx/wrapper.h")
+            .allowlist_item("I?ADLX\\w+")
+            .generate()
+            .expect("failed to generate adlx bindings");
+    
+        // Write the bindings to the $OUT_DIR/bindings.rs file.
+        let out_path = PathBuf::from("src/adlx/");
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("Couldn't write bindings!");
+    }
 }
