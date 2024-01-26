dc5a401e591bbe9c6dead6565c1aa0b7de4bcdb7
KYovchevski <kamen.yovchevski@traverseresearch.nl>
2024-01-26 14:20:01 +0100

    Introduce api-gen crate

--- adlx/src/ffi.rs
+++ adlx/src/ffi.rs
@@ -3,4 +3,4 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
 
-include!(concat!("adlx/", "/bindings.rs"));
+include!("bindings.rs");
---
+++ api-gen/build.rs
@@ -0,0 +1,17 @@
+fn main() {
+    use std::path::PathBuf;
+
+    println!("cargo:rerun-if-changed=src/adlx/wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("src/adlx/wrapper.h")
+        .allowlist_item("I?ADLX\\w+")
+        .generate()
+        .expect("failed to generate adlx bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from("../adlx/src/");
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
