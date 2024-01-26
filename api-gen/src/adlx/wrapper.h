98f0fe192f5fdd59ee4583194ad35b59eeeb460d
KYovchevski <kamen.yovchevski@traverseresearch.nl>
2024-01-26 16:00:37 +0100

    Move ADLX to vendor/

--- api-gen/src/adlx/wrapper.h
+++ api-gen/src/adlx/wrapper.h
@@ -1,6 +1,6 @@
-#include "../../../adlx/SDK/Include/ADLX.h"
+#include "../../../vendor/adlx/SDK/Include/ADLX.h"
 
 // Interfaces to generate bindings for
-#include "../../../adlx/SDK/Include/ISystem1.h"
-#include "../../../adlx/SDK/Include/IPerformanceMonitoring.h"
-#include "../../../adlx/SDK/Include/IPerformanceMonitoring1.h"
+#include "../../../vendor/adlx/SDK/Include/ISystem1.h"
+#include "../../../vendor/adlx/SDK/Include/IPerformanceMonitoring.h"
+#include "../../../vendor/adlx/SDK/Include/IPerformanceMonitoring1.h"
